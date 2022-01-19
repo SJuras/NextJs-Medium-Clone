3d9e0fdc336c52c2a8d4a6bf9e3bb6d253a95cb8
SJuras <jurasstpko@gmailcom>
2022-01-19 18:51:04 +0100

    v1.1.3 working on the hero section.

--- netfarce/pages/index.tsx
+++ netfarce/pages/index.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>NetFarce | Developers Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -11,6 +11,17 @@ export default function Home() {
 
       <Header />
 
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is a place for write, read and connect</h1>
+          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      {/* Posts */}
     </div>
-  )
+  );
 }
