1239df21b9baee85668a9b279ccdac92754138a6
SJuras <jurasstpko@gmailcom>
2022-01-26 19:16:49 +0100

    v1.1.4 custom font added.

--- netfarce/pages/index.tsx
+++ netfarce/pages/index.tsx
@@ -5,19 +5,19 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>NetFarce | Developers Blog</title>
+        <title>NetFarce | Tech Learning Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
 
-      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-        <div className="px-10 space-y-5">
-          <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is a place for write, read and connect</h1>
-          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
-        </div>
-        <div>
+      <div className="flex flex-col md:flex-row justify-around items-center bg-yellow-400 border-y border-black py-14">
+        <div className="px-6 space-y-5">
 
+        </div>
+        <div className="px-10 space-y-5">
+          <h1 className="text-center text-5xl md:text-left md:text-6xl max-w-xl font-Lustra font-bold">NETFARCE</h1>
+          <h2 className="text-3xl max-w-xl font-Lustra font-bold">CyberSecurity, Blockchain, Crypto, Neural Networks</h2>
         </div>
       </div>
 
