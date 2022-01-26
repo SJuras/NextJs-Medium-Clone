9729e78a93d0ea8d64a43eb5e62ad2be0d3bbdd3
SJuras <jurasstpko@gmailcom>
2022-01-27 00:38:33 +0100

    v1.1.8 working on about me section of About page.

--- netfarce/pages/about.tsx
+++ netfarce/pages/about.tsx
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Header from '../components/Header';
 
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 export default function About() {
   return(
@@ -20,14 +24,41 @@ export default function About() {
         </div>
 
         <div className="flex">
-          <img className="w-3/5 object-contain" src="/images/portrait.png" alt="stjepko juras" />
-        </div>
+          <div className="flex">
+            <img className="w-3/5 object-contain" src="/images/portrait.png" alt="stjepko juras" />
+          </div>
 
-        <div className="flex">
-          <h1 className="text-slate-50 font-Lustra">Creator:</h1>
-          <h1 className="text-slate-50 font-Lustra">Stjepko Juras</h1>
-          <FaMapMarkerAlt className="text-slate-50 my-1 text-xl"/>
-          <p className="text-slate-50 font-Lustra text-xl">Zagreb, Croatia</p>
+          <div className="flex flex-col">
+            <h1 className="text-slate-50 font-Lustra">Creator:</h1>
+            <h1 className="text-slate-50 font-Lustra">Stjepko Juras</h1>
+            <h1 className="text-slate-50 font-Lustra">Developer, Martial Arts Practitioner, Crypto Enthusiast</h1>
+            <a href="https://github.com/SJuras" className="">
+              <div className="flex group">
+                <FaLink className="text-slate-50 my-1 mr-2 text-xl transition duration-300 group-hover:text-red-600" />
+                <p className="text-slate-50 font-Lustra text-xl transition duration-300 group-hover:text-red-600">My Portfolio</p>
+              </div>
+            </a>
+            <a href="https://github.com/SJuras" className="">
+              <div className="flex group">
+                <FaGithub className="text-slate-50 my-1 mr-2 text-xl transition duration-300 group-hover:text-red-600" />
+                <p className="text-slate-50 font-Lustra text-xl transition duration-300 group-hover:text-red-600">Github</p>
+              </div>
+            </a>
+            <a href="https://github.com/SJuras" className="">
+              <div className="flex mb-2 group">
+                <FaFacebook className="text-slate-50 my-1 mr-2 text-xl transition duration-300 group-hover:text-red-600" />
+                <p className="text-slate-50 font-Lustra text-xl transition duration-300 group-hover:text-red-600">Facebook</p>
+              </div>
+            </a>
+            <div className="flex">
+              <FaMapMarkerAlt className="text-slate-50 my-1 mr-2 text-xl" />
+              <p className="text-slate-50 font-Lustra text-xl">Zagreb, Croatia</p>
+            </div>
+            <div className="flex">
+              <FaEnvelope className="text-slate-50 my-1 mr-2 text-xl" />
+              <p className="text-slate-50 font-Lustra text-xl">jurasstjepko@gmail.com</p>
+            </div>
+          </div>
         </div>
 
         <div className="flex">
@@ -37,6 +68,9 @@ export default function About() {
 
         <div className="flex">
           <p className="text-slate-50 font-Lustra text-xl">Description and a little bit about me.</p>
+          <p className="text-slate-50 font-Lustra text-xl">
+            Name Netfarce came from Tom Clancy's Net-Force, and it's awesome!
+          </p>
         </div>
 
         <div className="flex">
