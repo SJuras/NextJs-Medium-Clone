e711e64bb6b35bc72cb8e1ff4eddd9386dfcb5ce
SJuras <jurasstpko@gmailcom>
2022-02-04 18:54:41 +0100

    v1.1.9 about me page done.

--- netfarce/pages/about.tsx
+++ netfarce/pages/about.tsx
@@ -23,15 +23,31 @@ export default function About() {
           <h2 className="text-5xl md:text-6xl text-slate-50 font-Lustra">About</h2>
         </div>
 
-        <div className="flex">
-          <div className="flex">
+        <div className="flex flex-col md:flex-row items-start">
+          <div className="flex mb-4 md:mb-0 justify-center items-center">
             <img className="w-3/5 object-contain" src="/images/portrait.png" alt="stjepko juras" />
           </div>
 
           <div className="flex flex-col">
-            <h1 className="text-slate-50 font-Lustra">Creator:</h1>
-            <h1 className="text-slate-50 font-Lustra">Stjepko Juras</h1>
-            <h1 className="text-slate-50 font-Lustra">Developer, Martial Arts Practitioner, Crypto Enthusiast</h1>
+            <div className="flex flex-col mb-2">
+              <h1 className="text-slate-50 font-Lustra text-xl">Creator:</h1>
+              <h1 className="text-slate-50 font-Lustra text-2xl">Stjepko Juras</h1>
+            </div>
+            <h1 className="text-slate-50 font-Lustra text-lg mb-4">Developer, Martial Arts Practitioner, Crypto Enthusiast</h1>
+            <h1 className="text-slate-50 font-Lustra text-xl mb-2">About Me:</h1>
+            <p className="text-slate-50 font-Lustra text-lg">
+              Full-stack web developer, MERN wrangler transitioning to Web3.0
+            </p>
+            <p className="text-slate-50 font-Lustra text-lg">
+              Eager to learn all there is to know about inner workings of Blockchain technology,
+              Neural Networks and OpenCV.
+            </p>
+            <p className="text-slate-50 font-Lustra text-lg">
+              Life-long fan of cyberpunk genre.
+            </p>
+            <p className="text-slate-50 font-Lustra text-lg mb-4">
+              Name of the blog came from Tom Clancy's "Net-Force", cause Net-Force is awesome!
+            </p>
             <a href="https://github.com/SJuras" className="">
               <div className="flex group">
                 <FaLink className="text-slate-50 my-1 mr-2 text-xl transition duration-300 group-hover:text-red-600" />
@@ -59,22 +75,6 @@ export default function About() {
               <p className="text-slate-50 font-Lustra text-xl">jurasstjepko@gmail.com</p>
             </div>
           </div>
-        </div>
-
-        <div className="flex">
-          <h1 className="text-slate-50 font-Lustra">Creator:</h1>
-          <h1 className="text-slate-50 font-Lustra">Developer, Martial Arts Practitioner, Crypto Enthusiast</h1>
-        </div>
-
-        <div className="flex">
-          <p className="text-slate-50 font-Lustra text-xl">Description and a little bit about me.</p>
-          <p className="text-slate-50 font-Lustra text-xl">
-            Name Netfarce came from Tom Clancy's Net-Force, and it's awesome!
-          </p>
-        </div>
-
-        <div className="flex">
-
         </div>
 
       </div>
